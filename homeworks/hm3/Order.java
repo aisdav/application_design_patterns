2517852b38cc98df698fc03810eb8a952ae3efed
Davletkan Aisultan <aisdavletkan@gmail.com>
2026-02-08 14:05:29 +0500

    Create Order class with product details

---
+++ homeworks/hm3/Order.java
@@ -0,0 +1,19 @@
+public class Order {
+    private String productName;
+    private int quantity;
+    private double price;
+
+    public Order(String productName, int quantity, double price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
